c50ac42e9d999db77b9dcfe3607a73effa44932d
Amit <84635639+Amit-Web-bot@users.noreply.github.com>
2023-09-13 15:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+var dict = {}
+for (let ind =0; ind<=input.length-1; ind++){
+  if(dict[input[ind]]==undefined){
+    dict[input[ind]]=1;
+  }
+  else{
+    dict[input[ind]]+=1;
+  }
+}
+console.log(dict);
+var flag =0;
+for (var key in dict){
+  if(dict[key] ==1){
+	  input =key;
+    console.log(key);
+    flag=1;
+  }
+}
+if(flag==0){
+	input="null"
+  console.log("null")
+}
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
